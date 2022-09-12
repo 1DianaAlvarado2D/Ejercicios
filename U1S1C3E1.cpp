1128a8862a0c112e22fa77ab3a7da484e27d4daf
1DianaAlvarado2D <113377089+1DianaAlvarado2D@users.noreply.github.com>
2022-09-12 11:57:44 -0600

    Add files via upload

---
+++ U1S1C3E1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+  int n1, n2;
+  cout << "Ingrese el primer número: ";
+  cin >> n1;
+  cout << "Ingrese el segundo número: ";
+  cin >> n2;
+  if (n1 > n2){
+    cout << "El número" << n1 << " es mayor que el número " << n2 << endl;
+  }
+}
